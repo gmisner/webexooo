c9e3268554d898bf0a629b8f3cd5306af3b8ad40
gmisner <gmisner@live.com>
2022-06-25 15:13:28 -0700

    cleaned up code with IDE....

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ import time
 import json
 import sys
 import re
-import 
 
 # Suppress error messages 
 sys.stderr = open(os.devnull, "w")
-​
+
 # Create log file
 log_file = open("/tmp/webexOOO.log", "w")
 log_file.close()
-​
+
 # Authorization token(s)
 webex_personal_token = "xxx1" # For option #1, #2 and #3
 # webex_bot_token = "xxx2" # For option #1 and #2
@@ -37,17 +36,14 @@ def func0():
 # 'Authorization': 'Bearer ' + webex_integration token, # For option #3
     'Content-Type': 'application/json'
     }
-​
     response = requests.request("GET", url, headers=headers)
     r_json = response.json()
     message = r_json["items"][1]["html"]
-​
     TAG_RE = re.compile(r'<[^>]+>')
     message_parsed = TAG_RE.sub('', message)
-​
+
     # Twilio Integration
     client = Client(twilio_account_sid, twilio_auth_token)
-​
     message = client.api.account.messages.create(
         to="+1xxxxxxxxx",
         from_="+1yyyyyyyyy",
@@ -57,14 +53,12 @@ def func0():
 def func1():
     #print("\n| --> Create ngrok tunnel")
     #ngrok.connect(5000, bind_tls=True)
-​
     #tunnels = ngrok.get_tunnels()
     #tunnel_IP = re.findall(r'\"(.+?)\"', str(tunnels))[0]
-​
+
     # Create webex webhook
     print("| --> Create webex webhook")
     url = "https://webexapis.com/v1/webhooks"
-​
     payload = json.dumps({
     "resource": "messages",
     "event": "created",
@@ -78,7 +72,6 @@ def func1():
     }
     response = requests.request("POST", url, headers=headers, data=payload)
     r_json = response.json()
-​
     print("| --> Listening...")
   
  def func2():
@@ -90,18 +83,14 @@ def func1():
             if "localhost:5000" in result:
                 file.truncate(0)
                 file.write(str('rolloverLine'))
-​
                 url = "http://localhost:4040/api/requests/http?limit=1"
                 response = requests.request("GET", url)
                 r_json = response.json()
-​
                 for i in range(0, len(r_json['requests'])):
                     encrypted_response = r_json["requests"][i]["request"]["raw"]
-​
                     base64_bytes = encrypted_response.encode("ascii")
                     sample_string_bytes = base64.b64decode(base64_bytes)
                     sample_string = sample_string_bytes.decode("ascii")
-​
                     match = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', sample_string)
 
                     if str(match).strip("[']") == customer_email:
@@ -112,10 +101,8 @@ def func1():
                         log_file = open("/tmp/webexOOO.log", "a")
                         log_file.write(str(match).strip("[']")+ "\n")
                         log_file.close()
-​
                         with open("/tmp/webexOOO.log") as f:
                             personEmail = f.readlines()[-1]
-​
                         url = "https://webexapis.com/v1/messages"
                         payload = json.dumps({
                         "toPersonEmail": "" + str(personEmail) + "",
@@ -130,7 +117,6 @@ def func1():
                         response = requests.request("POST", url, headers=headers, data=payload)
             else:
                 pass
-​
 if __name__ == '__main__':
     Thread(target = func1).start()
     Thread(target = func2).start()
